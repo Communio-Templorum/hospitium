4f8980e44331cb4f6ff01f86f7ab16832f3cccc8
Sam Grundman <samuel@yodas.ws>
2021-08-30 10:44:19 -0400

    Add outline for Articles of Incorporation

---
+++ src/pages/articles/ctrl.js
@@ -0,0 +1,7 @@
+yodasws.page('pageArticles').setRoute({
+	title: 'Articles of Incorporation',
+	template: 'pages/articles/articles.html',
+	canonicalRoute: '/articles/',
+	route: '/articles/?',
+}).on('load', () => {
+});
